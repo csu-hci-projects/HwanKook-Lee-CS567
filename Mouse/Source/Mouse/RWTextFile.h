2ae04e66908fd433ae9f8a268bcff64c34684ba8
hwankook <hwankooklee@gmail.com>
2019-11-18 15:19:34 -0700

    Not Using

---
+++ Mouse/Source/Mouse/RWTextFile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RWTextFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOUSE_API URWTextFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
